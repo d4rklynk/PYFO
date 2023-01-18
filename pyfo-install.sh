7cc7f2d41b5354d50e55552cdd306c5dde190353
samsepi0l <74207682+d4rklynk@users.noreply.github.com>
2023-01-18 15:54:14 +0100

    Update pyfo-install.sh

--- pyfo-install.sh
+++ pyfo-install.sh
@@ -15,23 +15,24 @@ sudo dnf install -y dialog
 fi
 
 OPTIONS=(1 "Update your system - Do that first if you did not already"
-         2 "Enable AutoUpdates - Recommended"        
-         3 "Install Basic Software - check basic-softwares file for more information"
+         2 "Speed up DNF - This enables fastestmirror, max downloads and deltarpms""Enable AutoUpdates - Recommended"     
+         3 "Enable AutoUpdates - Recommended"
          4 "Enable RPM Fusion - Enables the RPM Fusion repos for your specific version"
          5 "Update Firmware - If your system supports fw update delivery"
-         6 "Speed up DNF - This enables fastestmirror, max downloads and deltarpms"
-         7 "Enable Flatpak - Enables the Flatpak repo and installs packages"
-         8 "Install some flatpak software - Check flaptak-packages.txt"
-         9 "Install Extras Software - Installs a bunch of my most used software"
+         6 "Install Basic Software - check basic-softwares file for more information"
+         7 "Install Extras Software - Installs a bunch of my most used software"
+         8 "Enable Flatpak - Enables the Flatpak repo and installs packages"
+         9 "Install some flatpak software - Check flaptak-packages.txt"
          10 "Install Brave - Recommended, installed from official repo"
          11 "Install Videos packages - Video codec and stuff as per the official doc"
          12 "Install Oh-My-ZSH - ZSH will be also installed"
          13 "Install minimal Oh-My-ZSH plugins"
-         14 "Install Nvidia - Install akmod nvidia drivers"
-         15 "Install linux-hardened - A linux hardened package from my COPR repo"
-         16 "Install hardened_malloc - A hardened_malloc package for fedora"
-         17 "Set default for hardened_malloc - If you don't know, do nothing"
-         18 "More hardening tweaks - NTS time, umask, firewall"
+         14 "Install Nvidia - Install akmod nvidia drivers and CUDA"
+         15 "Install linux-hardened - NOT NVIDIA USER - A linux hardened package from my COPR repo"
+	 16 "Harden your Fedora - Download kicksecure files, blacklisting unused modules, harden boot"
+         17 "Install hardened_malloc - A hardened_malloc package for fedora"
+         18 "Set default for hardened_malloc - If you don't know, do nothing"
+         19 "More hardening tweaks - NTS time, umask, firewall"
          98 "Reboot your system"
 	 99 "Quit")
 
@@ -54,15 +55,17 @@ while [ "$CHOICE -ne 4" ]; do
             notify-send "System updated - Reboot now" --expire-time=10
             ;;
         2)
+            echo "Speeding Up DNF"
+            echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+            echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+            echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+            notify-send "Your DNF config has now been amended" --expire-time=10
+           ;;
+        3)
             echo "Enable AutoUpdates"
             sudo dnf install dnf-automatic
             sudo systemctl enable --now dnf-automatic-install.timer
             notify-send "System updated - Reboot now" --expire-time=10
-            ;;
-        3)
-            echo "Install Basic Software"
-            sudo dnf install -y $(cat basic-dnf.txt)
-            notify-send "Basic Software have been installed" --expire-time=10
             ;;
         4)  
             echo "Enabling RPM Fusion"
@@ -81,28 +84,26 @@ while [ "$CHOICE -ne 4" ]; do
             sudo fwupdmgr update
             ;;
         6)
-            echo "Speeding Up DNF"
-            echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
-            echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-            echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
-            notify-send "Your DNF config has now been amended" --expire-time=10
-           ;;
-        7)
+            echo "Install Basic Software"
+            sudo dnf install -y $(cat basic-dnf.txt)
+            notify-send "Basic Software have been installed" --expire-time=10
+            ;;
+        7)  
+            echo "Installing Extras Software"
+            sudo dnf install -y $(cat extras-dnf.txt)
+            notify-send "Extras Software have been installed" --expire-time=10
+            ;;
+        8)
             echo "Enabling Flatpak"
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
             flatpak update
             flatpak install flathub com.github.tchx84.Flatseal
             notify-send "Flatpak has now been enabled and Flatseal is installed" --expire-time=10
             ;;
-        8)
+        9)
             echo "Install some flatpak software"
             source 'flatpak-install.sh'
             ;;    
-        9)  
-            echo "Installing Extras Software"
-            sudo dnf install -y $(cat extras-dnf.txt)
-            notify-send "Extras Software have been installed" --expire-time=10
-            ;;
         10)  
             echo "Installing Brave"
             sudo dnf install -y dnf-plugins-core
@@ -136,27 +137,32 @@ while [ "$CHOICE -ne 4" ]; do
         14)  
             echo "Installing Nvidia Driver Akmod-Nvidia"
             sudo dnf install -y akmod-nvidia
+	    sudo dnf install xorg-x11-drv-nvidia-cuda
             notify-send "All done" --expire-time=10
 	       ;;
         15)
-            echo "Hardening kernel"
+            echo "Install kernel-hardened"
             sudo dnf copr enable samsepi0l/HardHatOS
             sudo dnf install -y kernel-hardened
             sudo bash -c 'echo "kernel.unprivileged_userns_clone=1" > /etc/sysctl.d/10_kernel.unprivileged_userns_clone.conf'
-            notify-send "Kernel is hardened (you must reboot to make it effective)" --expire-time=10
+            notify-send "kernel-hardened has been installed (you must reboot to make it effective)" --expire-time=10
            ;;
-        16)
+	16)
+	    echo "Hardening Fedora [WIP]"
+	    echo "WIP"
+	    notify-send "Fedora is hardened (you must reboot to make it effective)" --expire-time=10
+        17)
             echo "Installing hardened_malloc"
             sudo dnf copr enable samsepi0l/HardHatOS
             sudo dnf install -y hardened_malloc
             notify-send "hardened_malloc installed (you must reboot to make it effective)" --expire-time=10
            ;;
-        17)
+        18)
             echo "Set hardening_malloc to default"
             sudo bash -c 'echo "libhardened_malloc.so" > /etc/ld.so.preload '
             notify-send "hardening_malloc has been set to default (you must reboot to make it effective)" --expire-time=10
            ;;
-        18)
+        19)
             echo "Set umask to 077 for all users instead of 022"
             sudo bash -c 'echo "umask 077" >  cat /etc/profile.d/set-umask077-for-all-users.sh'
             echo "Set firewall to drop zone"
